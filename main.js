50df6ba11d4e0bfde4719ef5efcaf81f59cb8adf
Vladlen <v.shapoval.7@gmail.com>
2026-09-25 12:05:55 +0200

    Implemented UnityKernel page and improved sidebar

--- main.js
+++ main.js
@@ -5,4 +5,10 @@ if (btn && sidebar) {
     btn.addEventListener("click", () => {
         sidebar.classList.toggle("active");
     });
+    document.addEventListener("click", (e) => {
+        const target = e.target;
+        if (!sidebar.contains(target) && target !== btn) {
+            sidebar.classList.remove("active");
+        }
+    });
 }
--- main.ts
+++ main.ts
@@ -5,4 +5,11 @@ if (btn && sidebar) {
     btn.addEventListener("click", () => {
         sidebar.classList.toggle("active");
     });
+    document.addEventListener("click", (e) => {
+        const target = e.target as Node;
+
+        if (!sidebar.contains(target) && target !== btn) {
+            sidebar.classList.remove("active");
+        }
+    });
 }
